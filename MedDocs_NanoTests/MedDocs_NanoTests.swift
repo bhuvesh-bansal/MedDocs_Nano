8f2da64205b7c7ec30813cd9462108137a91118f
Bhuvesh <bansalbansal511@gmail.com>
2025-01-15 14:34:40 +0530

    Initial Commit

---
+++ MedDocs_NanoTests/MedDocs_NanoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MedDocs_NanoTests.swift
+//  MedDocs_NanoTests
+//
+//  Created by Bhuvesh Bansal on 15/01/25.
+//
+
+import Testing
+@testable import MedDocs_Nano
+
+struct MedDocs_NanoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
