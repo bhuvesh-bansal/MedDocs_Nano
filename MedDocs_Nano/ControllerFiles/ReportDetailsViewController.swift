c399277fa9464043cce6ec17b4f5b0bc0d671ae9
Bhuvesh bansal <96925288+bhuvesh-bansal@users.noreply.github.com>
2025-01-16 09:39:06 +0530

    Solved the conflicts

---
+++ MedDocs_Nano/ControllerFiles/ReportDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ReportDetailsViewController.swift
+//  MedDocs_Nano
+//
+//  Created by Bhuvesh Bansal on 15/01/25.
+//
+
+import UIKit
+
+class ReportDetailsViewController: UIViewController {
+
+    @IBOutlet weak var medicationTableView: UITableView!
+    @IBOutlet weak var reportsCollectionView: UICollectionView!
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
